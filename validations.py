6710ca46c3c704605a399d8fb681c61c316713ee
egowar <38001437+egowar@users.noreply.github.com>
2025-02-10 19:13:38 +0100

    Update validations.py
    Fix misso

--- validations.py
+++ validations.py
@@ -2,11 +2,11 @@
 
 
 def validate_user(username, minlen):
-    """Checks if the recieved username matches the required conditions."""
+    """Checks if the received username matches the required conditions."""
     if type(username) != str:
-        raise TypeError("username must be a string")
+        raise TypeError("username must be a string").
     if minlen < 1:
-        raise ValueError("minlen must at least 1")
+        raise ValueError("minlen must at least 1").
         
     if len(username) < minlen:
         ereturn False
